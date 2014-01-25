dce2d214425ad6ddc181fb71b7b63319a9e256ba
Roy Zheng <roycraft3@gmail.com>
2014-01-25 05:13:43 -0500

    code

--- server/www/index.php
+++ server/www/index.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!isset($_POST['action'])) {
-    return -2;
+return -2;
 }
 $db = mysqli_connect('localhost', 'root', 'happyhacking', 'fistbump');
 if (mysqli_connect_errno($db)) {
@@ -11,10 +11,16 @@ if (mysqli_connect_errno($db)) {
 if ($_POST['action'] == 'newusr') {
     if (!isset($_POST['usr']) || !isset($_POST['pwd']) || !isset($_POST['email'])) {
         //handle error here
+echo 'not set';
         return -1;
     }
     //check if exists but meh
-    $db->query("INSERT INTO main(usr,pwd,email) VALUES (" . $_POST['usr'] . "," . $_POST['pwd'] . "," . $_POST['email'] . ")");
+$stmt = "INSERT INTO main(usr,pwd,email) VALUES ('$_POST[usr]',"."'$_POST[pwd]',"."'$_POST[email]'
+if(!    $db->query($stmt)){
+return -1;
+}
+else{
     return 0;
+}
 }
 ?>
