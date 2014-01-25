899f2e496e86e08748069cfe82dd46108a704eee
Roy Zheng <roycraft3@gmail.com>
2014-01-25 15:52:18 -0800

    geodistance done

--- server/www/geo.php
+++ server/www/geo.php
@@ -11,23 +11,28 @@ if (isset($_POST['lon']) && isset($_POST['lat']) && isset($_POST['usr']) && isse
     return;
 }
 if (isset($_GET['lon']) && isset($_GET['lat'])) {
-    $spread_m = 500;
+    $spread_m = 300;
     $earth_r = 6378137;
-
-    $spread_lat = 180 / pi * $spread_m / $earth_r;
-    $spread_long = 180 / pi * $spread_m / ($earth_r * cos(pi() * $_GET['lat'] / 180));
+    $spread_lat = 180 / pi() * $spread_m / $earth_r;
+    $spread_long = 180 / pi() * $spread_m / ($earth_r * cos(pi() * $_GET['lat'] / 180));
 
     $long_min = $_GET['lon'] - $spread_long;
     $long_max = $_GET['lon'] + $spread_long;
-    $lat_min = $_GET['lat'] + $spread_lat;
+    $lat_min = $_GET['lat'] - $spread_lat;
     $lat_max = $_GET['lat'] + $spread_lat;
-    $result = $db->query("SELECT * FROM geo WHERE long < $long_max AND long > $long_min AND lat < $lat_max AND lat > $lat_min");
-    if (!$result)
+    
+    $db = mysqli_connect('localhost', 'root', 'happyhacking', 'fistbump');
+    $q="SELECT * FROM geo WHERE lon BETWEEN $long_min AND $long_max AND lat BETWEEN $lat_min AND $lat_max";
+echo $q;
+    $result = $db->query($q);
+    
+    if (!$result) {
         echo $db->error;
+    }
     $return = [];
     while ($row = mysqli_fetch_assoc($result)) {
         $return[] = $row;
     }
-    echo $return;
+    echo json_encode($return);
 }
 ?>
