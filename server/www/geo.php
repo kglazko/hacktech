73fd48f9fd6115ece8776e05add0be1798d2426d
Yiwei Zheng <yiweizhe@usc.edu>
2014-01-25 12:31:02 -0800

    reverse haversine equation done

--- server/www/geo.php
+++ server/www/geo.php
@@ -7,16 +7,21 @@ if (isset($_POST['long']) && isset($_POST['lat']) && isset($_POST['usr']) && iss
 }
 if (isset($_GET['long']) && isset($_GET['lat'])) {
     $spread_m = 500;
-    $spread_deg = $spread_m / 111111;
-    $long_min = $_GET['long'] - $spread_deg;
-    $long_max = $_GET['long'] + $spread_deg;
-    $lat_min = $_GET['lat'] - $spread_deg;
-    $lat_max = $_GET['lat'] + $spread_deg;
+    $earth_r = 6378137;
+//    $spread_deg = $spread_m / 111111;
+    
+    $spread_lat = 180/pi*$spread_m/$earth_r;
+    $spread_long = 180/pi*$spread_m/($earth_r*cos(pi*$_GET['lat']/180));
+    
+    $long_min = $_GET['long'] - $spread_long;
+    $long_max = $_GET['long'] + $spread_long;
+    $lat_min = $_GET['lat'] + $spread_lat;
+    $lat_max = $_GET['lat'] + $spread_lat;
     $result = $db->query("SELECT * FROM geo WHERE long < $long_max AND long > $long_min AND lat < $lat_max AND lat > $lat_min");
     $return=[];
     while ($row = mysqli_fetch_assoc($result)) {
         $return[] = $row;
     }
-    return $return;
+    echo $return;
 }
 ?>
