561dac7c03e7299a7e40945477192c4cc15e3a53
Jack Wong <yamijack2000@gmail.com>
2014-01-25 11:48:38 -0800

    Add LocationActivity and LoginActivity

---
+++ FistBump/src/io/hacktech/fistbump/LoginActivity.java
@@ -0,0 +1,10 @@
+package io.hacktech.fistbump;
+
+import android.os.Bundle;
+
+public class LoginActivity extends BaseActivity {
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_login);
+	}
+}
--- FistBump/src/io/hacktech/fistbump/MainActivity.java
+++ FistBump/src/io/hacktech/fistbump/MainActivity.java
@@ -1,11 +1,37 @@
 package io.hacktech.fistbump;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		bindLoginButton();
+		bindFindUsersButton();
+	}
+	
+	private void bindLoginButton() {
+		Button button = (Button)findViewById(R.id.login_btn);
+		button.setOnClickListener(new Button.OnClickListener() {
+		    public void onClick(View v) {
+				Intent intent = new Intent(getBaseContext(), LoginActivity.class);
+				startActivity(intent);
+		    }
+		});
+	}
+	
+	private void bindFindUsersButton() {
+		Button buttonOne = (Button)findViewById(R.id.find_users_btn);
+		buttonOne.setOnClickListener(new Button.OnClickListener() {
+		    public void onClick(View v) {
+				Intent intent = new Intent(getBaseContext(), LocationActivity.class);
+				startActivity(intent);
+		    }
+		});
 	}
 }
