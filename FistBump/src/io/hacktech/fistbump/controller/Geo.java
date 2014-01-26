6e6dd0c235b294f0687555e45e6adfdd68680a71
Roy Zheng <roycraft3@gmail.com>
2014-01-25 16:56:03 -0800

    geo lib

---
+++ FistBump/src/io/hacktech/fistbump/controller/Geo.java
@@ -0,0 +1,20 @@
+package io.hacktech.fistbump.controller;
+
+public class Geo {
+	public static double getDistanceFromLatLonInKm(double lat1, double lon1,
+			double lat2, double lon2) {
+		double R = 6378.137f; // Radius of the earth in km
+		double dLat = Geo.deg2rad(lat2 - lat1); // deg2rad below
+		double dLon = Geo.deg2rad(lon2 - lon1);
+		double a = Math.sin(dLat / 2) * Math.sin(dLat / 2)
+				+ Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
+				* Math.sin(dLon / 2) * Math.sin(dLon / 2);
+		double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+		double d = R * c; // Distance in km
+		return d;
+	}
+
+	public static double deg2rad(double deg) {
+		return deg * (Math.PI / 180);
+	}
+}
