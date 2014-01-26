1d489450abfa95aa73a3f867ee6eee5c785145bc
Jack Wong <yamijack2000@gmail.com>
2014-01-25 22:05:29 -0800

    Rename LocationActivity

--- FistBump/src/io/hacktech/fistbump/LocationSearchLandingActivity.java
+++ FistBump/src/io/hacktech/fistbump/LocationSearchLandingActivity.java
@@ -1,11 +1,15 @@
 package io.hacktech.fistbump;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-public class LocationActivity extends BaseActivity {
+public class LocationSearchLandingActivity extends BaseActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_location);
+		
+		Intent intent = new Intent(getBaseContext(), SearchResultsActivity.class);
+		startActivity(intent);
 	}
 }
